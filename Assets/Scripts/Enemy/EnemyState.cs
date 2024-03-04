d5eff602961e321048b98dadfedc4314b4cb6f49
Andrea Mendez <andreacelestem11@gmail.com>
2024-03-03 23:40:11 -0600

    progress on Enemy behaviors

---
+++ Assets/Scripts/Enemy/EnemyState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyStateID{
+    Wander
+}
+public interface EnemyState
+{
+    EnemyStateID GetID();
+    void Enter(EnemyController enemy);
+    void Update(EnemyController enemy);
+    void Exit(EnemyController enemy);
+}
