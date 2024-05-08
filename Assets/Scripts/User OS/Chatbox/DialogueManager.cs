7b7261a2c393bee9fc35839dbef3e35e901df759
Andrea Mendez <andreacelestem11@gmail.com>
2024-05-08 13:45:36 -0500

    fixed some weird intellisense issues
    finished fr,,,, for now,,, but fr

--- Assets/Scripts/User OS/Chatbox/DialogueManager.cs
+++ Assets/Scripts/User OS/Chatbox/DialogueManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security;
-using UnityEditor.VersionControl;
 using UnityEngine;
 
 [System.Serializable]
