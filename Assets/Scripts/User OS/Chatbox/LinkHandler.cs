d19e347e762f5c2673344c100eb948488ab8712f
Andrea Mendez <andreacelestem11@gmail.com>
2024-05-30 17:37:33 -0500

    working on progression

--- Assets/Scripts/User OS/Chatbox/LinkHandler.cs
+++ Assets/Scripts/User OS/Chatbox/LinkHandler.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class LinkHandler : MonoBehaviour
-{
-    [SerializeField] private GameObject browserWindow;
+public class LinkHandler : MonoBehaviour{
     [SerializeField] private TextMeshProUGUI linkTxt;
+    private UserManager userManager;
+    private GameObject browser;
+    private BrowserManager loader;
+    void Awake(){
+        userManager = GameObject.FindGameObjectWithTag("GM").GetComponent<UserManager>();
 
+    }
     public void OpenLinkInBrowser(){
+        userManager.LoadBrowserWindow();
+        loader = GameObject.FindGameObjectWithTag("BrowserWindow").GetComponent<BrowserManager>();
         string link = linkTxt.text;
-        GameObject newWindow = Instantiate(browserWindow, transform.root,false);
-        BrowserManager tmpLoader = newWindow.GetComponent<BrowserManager>();
-        tmpLoader.BrowserSearch(link);
+        loader.BrowserSearch(link);
     }
 }
